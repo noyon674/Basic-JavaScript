f6a9e839d0a3eb7fda2c36dc748e67a4422d40a1
Noyon sarker <124178299+noyon674@users.noreply.github.com>
2023-02-14 07:06:00 -0800

    Add files via upload

---
+++ Error/script.js
@@ -0,0 +1,27 @@
+////////////////////////// Error Handlling
+/*
+try -catch
+*/
+
+try{
+    Love('Love is a Horror word.')
+}
+catch(err){
+    console.log(err.message)
+}
+
+//////// Error throwing
+
+let x = '52';
+try{
+    if(x == '') throw 'Empty';
+    if(x <= 5) throw 'Lowest';
+    if(x > 5) throw 'highest';
+}
+catch(pick){
+    console.log(pick);
+}
+
+
+///////////////////
+
